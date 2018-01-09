5d6333acad8adc50917c88c4159202e2b06bceb2
Ali <Ali@DESKTOP-5U6P67U>
2018-01-09 17:18:37 +0100

    test

--- src/main/resources/static/js/student.js
+++ src/main/resources/static/js/student.js
@@ -4,7 +4,24 @@
 
 /** Método que activa el elemento del listado seleccionado **/
 
-$("li").click(function() {
-  $("li").removeClass("active");
-  $(this).addClass("active");
-});
+
+/** $("li").click(function(e) {
+	  $("li").removeClass("active");
+	  $(this).addClass("active");
+	  var id = e.target.id;
+	  if ((id == "newstudent") && ($("#div"+id).hasClass('hidden'))){
+		  $("#div"+id).removeClass('hidden');
+		  $("#div"+id).addClass('show');
+		  $("#divliststudent").removeClass('show');
+		  $("#divliststudent").addClass('hidden');
+		  
+	  }  else if  ($("#div"+id).hasClass('hidden')){
+		  $("#div"+id).removeClass('hidden');
+		  $("#div"+id).addClass('show');
+		  $("#divnewstudent").removeClass('show');
+		  $("#divnewstudent").addClass('hidden');
+	  }
+
+});**/
+
+
